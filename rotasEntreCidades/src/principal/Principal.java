2a596af94a1dc6a8a4e702f3e940f7c393cb22ab
marciusOrigenes <marcius_servo@hotmail.com>
2017-04-04 23:39:52 -0300

    Add files via upload

---
+++ rotasEntreCidades/src/principal/Principal.java
@@ -0,0 +1,213 @@
+package principal;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import viaFerrea.Ferrovias;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner arquivo = null;
+
+		Set<Character> contaTotalCidades = new HashSet<Character>();
+
+		try {
+			Character i, j;
+			String aux;
+			int dist;
+			
+			arquivo = new Scanner(new FileReader("ferrovia.txt"));
+			
+			while(arquivo.hasNext()){
+				aux = arquivo.nextLine();
+				
+				i = aux.charAt(0);
+				j = aux.charAt(1);
+				dist = Integer.parseInt(aux.substring(2));
+				
+				contaTotalCidades.add(i);
+				contaTotalCidades.add(j);
+			}
+			
+		} catch(IOException e){
+			e.printStackTrace();
+			System.out.println("Erro na leitura do arquivo. Tente novamente!");
+			System.exit(1);
+			
+		} finally {
+			arquivo.close();
+		}
+
+		Ferrovias ferrovia = Ferrovias.getInstance(contaTotalCidades.size());
+
+		try {
+			Character i, j;
+			String aux;
+			int dist;
+			
+			arquivo = new Scanner(new FileReader("ferrovia.txt"));
+			
+			while(arquivo.hasNext()){
+				aux = arquivo.nextLine();
+				
+				i = aux.charAt(0);
+				j = aux.charAt(1);
+				dist = Integer.parseInt(aux.substring(2));
+				
+				ferrovia.setMatriz(i.charValue(), j.charValue(), dist);
+			}
+			
+		} catch(IOException e){
+			System.out.println("Erro na leitura do arquivo. Tente novamente!");
+			System.exit(1);
+			
+		} finally {
+			arquivo.close();
+		}
+		
+		ArrayList<Character> vet = new ArrayList<Character>();
+		
+		vet.add('A');
+		vet.add('B');
+		vet.add('C');		
+		if(ferrovia.distanciaDadoUmCaminho(vet) != -1)
+			System.out.println("Saída # 1: "+ferrovia.distanciaDadoUmCaminho(vet));
+		else
+			System.out.println("Saída # 1: NENHUMA ROTA ENCONTRADA");
+		
+		vet = new ArrayList<Character>();
+		
+		vet.add('A');
+		vet.add('D');		
+		if(ferrovia.distanciaDadoUmCaminho(vet) != -1)
+			System.out.println("Saída # 2: "+ferrovia.distanciaDadoUmCaminho(vet));
+		else
+			System.out.println("Saída # 2: NENHUMA ROTA ENCONTRADA");
+		
+		vet = new ArrayList<Character>();
+		
+		vet.add('A');
+		vet.add('D');
+		vet.add('C');
+		if(ferrovia.distanciaDadoUmCaminho(vet) != -1)
+			System.out.println("Saída # 3: "+ferrovia.distanciaDadoUmCaminho(vet));
+		else
+			System.out.println("Saída # 3: NENHUMA ROTA ENCONTRADA");
+		
+		vet = new ArrayList<Character>();
+		
+		vet.add('A');
+		vet.add('E');
+		vet.add('B');
+		vet.add('C');
+		vet.add('D');
+		if(ferrovia.distanciaDadoUmCaminho(vet) != -1)
+			System.out.println("Saída # 4: "+ferrovia.distanciaDadoUmCaminho(vet));
+		else
+			System.out.println("Saída # 4: NENHUMA ROTA ENCONTRADA");
+		
+		vet = new ArrayList<Character>();
+		
+		vet.add('A');
+		vet.add('E');
+		vet.add('D');
+		if(ferrovia.distanciaDadoUmCaminho(vet) != -1)
+			System.out.println("Saída # 5: "+ferrovia.distanciaDadoUmCaminho(vet));
+		else
+			System.out.println("Saída # 5: NENHUMA ROTA ENCONTRADA");
+		
+	   
+		int[][] mat = ferrovia.calculaMatrizQtdCaminhosPossiveis(2) ;
+		int soma = mat['C' - 65]['C' - 65];
+	    mat = ferrovia.calculaMatrizQtdCaminhosPossiveis(3) ;
+		soma += mat['C' - 65]['C' - 65];
+		System.out.println("Saída # 6: "+soma);
+		
+		mat = ferrovia.calculaMatrizQtdCaminhosPossiveis(4) ;
+		System.out.println("Saída # 7: "+mat['A' - 65]['C' - 65]);		
+		
+		int dist = ferrovia.calculaCaminhoMinimo('A', 'C');
+		System.out.println("Saída # 8: "+dist);
+		
+		dist = ferrovia.calculaCaminhoMinimo('B', 'B');
+		System.out.println("Saída # 9: "+dist);
+		
+		vet.clear();
+		int result = 0, cont = 0;
+		vet.add('C');
+		vet.add('D');
+		vet.add('C');
+		result += ferrovia.distanciaDadoUmCaminho(vet);
+		if(result <= 30){
+			cont++;
+		}
+		result = 0;
+		vet.clear();
+		vet.add('C');
+		vet.add('E');
+		vet.add('B');
+		vet.add('C');
+		result += ferrovia.distanciaDadoUmCaminho(vet);
+		if(result <= 30){
+			cont++;
+		}
+		result = 0;
+		vet.add('D');
+		vet.add('C');
+		result += ferrovia.distanciaDadoUmCaminho(vet);
+		if(result <= 30){
+			cont++;
+		}
+		result = 0;
+		vet.clear();
+		vet.add('C');
+		vet.add('D');
+		vet.add('C');
+		vet.add('E');
+		vet.add('B');
+		vet.add('C');
+		result += ferrovia.distanciaDadoUmCaminho(vet);
+		if(result <= 30){
+			cont++;
+		}
+		result = 0;
+		vet.clear();
+		vet.add('C');
+		vet.add('D');
+		vet.add('E');
+		vet.add('B');
+		vet.add('C');
+		result += ferrovia.distanciaDadoUmCaminho(vet);
+		if(result <= 30){
+			cont++;
+		}
+		result = 0;
+		vet.clear();
+		vet.add('C');
+		vet.add('E');
+		vet.add('B');
+		vet.add('C');
+		vet.add('E');
+		vet.add('B');
+		vet.add('C');
+		result += ferrovia.distanciaDadoUmCaminho(vet);
+		if(result <= 30){
+			cont++;
+		}
+		result = 0;
+		vet.add('E');
+		vet.add('B');
+		vet.add('C');
+		result += ferrovia.distanciaDadoUmCaminho(vet);
+		if(result <= 30){
+			cont++;
+		}
+		
+		System.out.println("Saída # 10: "+cont);
+	}
+	
+}
---
+++ rotasEntreCidades/src/viaFerrea/Ferrovias.java
@@ -0,0 +1,144 @@
+package viaFerrea;
+
+import java.util.ArrayList;
+
+public class Ferrovias{
+
+	private int[][] cidadesAdj;
+	private int totalCidades;
+	private static Ferrovias instance = null;
+	
+	private Ferrovias(int totCid){
+		this.totalCidades = totCid;
+		this.cidadesAdj = new int[totalCidades][totalCidades];
+	}
+	
+	/* Usando o padrão Singleton de Design Pattern, visto que em nosso contexto só existe uma ferrovia. */
+	public static Ferrovias getInstance(int totCid){
+		if(instance == null){
+			instance = new Ferrovias(totCid);
+		}
+		return instance;
+	}
+
+	/* Método para inserir valores na matriz */
+	public void setMatriz(char i, char j, int dist){
+
+		/* Pela tabela ASCII, se subtrairmos 65 do caracter 'A' chegamos ao valor 0, do 'B' chegamos ao valor 1 e assim sucessivamente.
+		 * Logo, linha 0 corresponde à origem da cidade 'A', linha 1 à origem da cidade 'B' e assim sucessivamente.
+		 */		
+		this.cidadesAdj[i - 65][j - 65] = dist;
+	}
+
+	/* Retorna a distancia dado um caminho fornecido. */
+	public int distanciaDadoUmCaminho(ArrayList<Character> vet){
+
+		int tam = 0, soma = 0;
+
+		while( tam < vet.size() - 1){	
+			
+			/* Pela tabela ASCII, se subtrairmos 65 do caracter 'A' chegamos ao valor 0, do 'B' chegamos ao valor 1 e assim sucessivamente.
+			 * Logo, linha 0 corresponde à origem da cidade 'A', linha 1 à origem da cidade 'B' e assim sucessivamente.
+			 * Na expressão abaixo, a linha é o primeiro elemento do vetor e a coluna o segundo, sofrendo incremento de 1.
+			 */	
+			soma += cidadesAdj[vet.get(tam) - 65][(vet.get(tam + 1) - 65)];
+			
+			if(cidadesAdj[vet.get(tam) - 65][(vet.get(tam + 1) - 65)] == 0)
+				return -1;
+
+			tam++;
+		}
+		return soma;
+	}
+
+	/* Eleva uma matriz à quantidade de passos a qual se deseja saber a quantidade de caminhos possíveis. */
+	public int[][] calculaMatrizQtdCaminhosPossiveis(int passos){
+
+		int[][] b = new int[totalCidades][totalCidades];
+		int[][] c =new int[totalCidades][totalCidades];
+
+
+
+		int[][] a= new int[totalCidades][totalCidades];
+
+		for(int i = 0; i < totalCidades; i++) {
+			for(int j = 0; j < totalCidades; j++) {
+				a[i][j] = cidadesAdj[i][j];
+			}
+		}
+		/* Tira os pesos da matriz que representa o grafo, transformando-a em uma matriz com somente 0s e 1s */
+		for(int i = 0; i < totalCidades; i++){
+			for(int j = 0; j < totalCidades; j++){
+				if(cidadesAdj[i][j] !=0)
+					a[i][j] = 1;
+			}
+		}
+
+		for(int i = 0; i < totalCidades; i++) {
+			for(int j = 0; j < totalCidades; j++) {
+				b[i][j] = a[i][j];
+			}
+		}
+		passos--;
+		
+		/* O código abaixo mostra uma multiplicação de matrizes sendo feita (passos - 1) vezes.
+		 * Ao término, teremos uma matriz da quantidade de caminhos possiveis em relação à quantidade de passos informada.
+		 * Ou seja, uma matriz² trará como resultado uma matriz onde em cada posição teremos a quantidade de caminhos possíveis com 2 passos.
+		 */
+		while(passos > 0){
+			for (int i = 0; i < totalCidades; i++)
+				for (int j = 0; j < totalCidades; j++)
+					for (int x = 0; x < totalCidades; x++) 
+						c[i][j] += a[i][x] * b[x][j];
+			passos--;
+			if(passos>0){
+				for(int i = 0; i < totalCidades; i++) {
+					for(int j = 0; j < totalCidades; j++) {
+						b[i][j] = c[i][j];
+					}
+				}
+				c =new int[totalCidades][totalCidades];
+			}
+		}	
+		return c;
+	}
+
+	public void imprime(int[][] a, int dim){
+
+		for (int i = 0; i < dim; i++){
+			System.out.println();
+			for (int j = 0; j < dim; j++)
+				System.out.print(a[i][j]+" ");
+		}
+		System.out.println();
+	}
+
+	/* Para calcular o caminho mínimo foi usado o Algoritmo de Floyd-Warshall, usando operações simples de matemática aplicadas à matriz de adjacências. */
+	public int calculaCaminhoMinimo(char orig, char dest){
+
+		int[][] b = new int[totalCidades][totalCidades];
+
+		for(int i = 0; i < totalCidades; i++) {
+			for(int j = 0; j < totalCidades; j++) {
+				b[i][j] = cidadesAdj[i][j];
+			}
+		}
+		
+		for(int i = 0; i < totalCidades; i++) 
+			for(int j = 0; j < totalCidades; j++)
+				if(b[i][j] == 0)
+					b[i][j] = 99999;
+
+		for (int k = 0; k < totalCidades; k++){
+			for (int i = 0; i < totalCidades; i++)
+				for (int j = 0; j < totalCidades; j++)
+					if(b[i][k] + b[k][j] < b[i][j])
+						b[i][j] = b[i][k] + b[k][j];
+		}
+
+		/* Pela tabela ASCII, se subtrairmos 65 do caracter 'A' chegamos ao valor 0, do 'B' chegamos ao valor 1 e assim sucessivamente.
+		 * Logo, linha 0 corresponde à origem da cidade 'A', linha 1 à origem da cidade 'B' e assim sucessivamente.
+		 */	
+		return b[orig - 65][dest - 65];
+	}
+}
